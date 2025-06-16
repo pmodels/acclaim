a0e4436839698e2da821fd626f0c21a00ff4cf14
mjwilkins18 <wilkins@anl.gov>
2025-06-16 19:40:18 +0000

    Updated aurora_xpu build script to 24.347

--- utils/osu/osu_build_aurora_xpu.sh
+++ utils/osu/osu_build_aurora_xpu.sh
@@ -7,6 +7,6 @@ mpicxx=$2
 
 pwd=$(pwd)
 autoreconf -fi # Aurora has an older version of autoconf/automake than OSU (1.15 vs. 1.16), so we force the reconf to workaround this
-./configure --prefix=${pwd}/build --enable-sycl --with-sycl=/opt/aurora/24.180.3/updates/oneapi/compiler/eng-20240629 CC=$mpicc CXX=$mpicxx
+./configure --prefix=${pwd}/build --enable-sycl --with-sycl=/opt/aurora/24.347.0/oneapi/compiler/latest CC=$mpicc CXX=$mpicxx
 make
 make install
